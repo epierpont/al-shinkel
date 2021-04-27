c60f82485eea0a2af9048fa3276e20b6e079fbc0
epierpont <epierpont@swiftcom.com>
2021-04-27 06:04:59 -0400

    Updated browser router

--- src/index.js
+++ src/index.js
@@ -2,12 +2,19 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 import "bootstrap/dist/js/bootstrap.js";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <Switch>
+        <Route exact path="/">
+          <App />
+        </Route>
+      </Switch>
+    </Router>
   </React.StrictMode>,
   document.getElementById("root")
 );
