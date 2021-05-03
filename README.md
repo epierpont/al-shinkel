c682a3ea48664d3095fc2a69df773ef49178d5d4
epierpont <epierpont@swiftcom.com>
2021-05-03 13:36:05 -0400

    Updated resume

--- README.md
+++ README.md
@@ -39,6 +39,10 @@ Instead, it will copy all the configuration files and the transitive dependencie
 
 You don’t have to ever use `eject`. The curated feature set is suitable for small and middle deployments, and you shouldn’t feel obligated to use this feature. However we understand that this tool wouldn’t be useful if you couldn’t customize it when you are ready for it.
 
+### `npm run deploy`
+
+This will trigger a build to the gh-pages branch, after the push the domain will have to be entered once more within Github Pages.
+
 ## Learn More
 
 You can learn more in the [Create React App documentation](https://facebook.github.io/create-react-app/docs/getting-started).
--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from "react";
-import { Helmet } from "react-helmet";
 import "./styles/base.scss";
 import Header from "./components/Header";
 import About from "./components/About";
@@ -7,32 +6,11 @@ import Callout from "./components/Callout";
 import Services from "./components/Services";
 import Contact from "./components/Contact";
 import Footer from "./components/Footer";
-import alShinkel from "./images/al-shinkel.png";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Helmet>
-          <meta charSet="utf-8" />
-          <title>
-            Dr. Al Shinkel, Ed.D. | Career Consultant | Biddeford Pool, Maine
-          </title>
-          <meta
-            name="description"
-            content="Dr. Al Shinkel specializes in career development services, career counseling, college counseling, life planning and special consulting services out of Biddeford Pool, ME."
-          />
-          <meta property="og:image" content={alShinkel} />
-          <meta
-            property="og:title"
-            content="Dr. Al Shinkel, Ed.D. | Career Consultant | Biddeford Pool, Maine"
-          />
-          <meta
-            property="og:description"
-            content="Dr. Al Shinkel specializes in career development services, career counseling, college counseling, life planning and special consulting services out of Biddeford Pool, ME."
-          />
-          <link rel="canonical" href="https://alshinkel.com/" />
-        </Helmet>
         <main>
           <Header />
           <About />
